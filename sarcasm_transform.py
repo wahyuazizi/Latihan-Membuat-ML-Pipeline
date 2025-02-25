97a3b893396affde7d784bbfe73416f0e7db8f0a
Azizi <wahyuazizi03@gmail.com>
2025-02-25 09:57:56 +0800

    update latihan

---
+++ sarcasm_transform.py
@@ -0,0 +1,24 @@
+
+import tensorflow as tf
+
+LABEL_KEY = 'is_sarcastic'
+FEATURE_KEY = 'headline'
+
+def transformed_name(key):
+    '''renaming transformed name feature'''
+    return key + '_xf'
+
+def preprocessing_fn(inputs):
+    """
+    Preprocess input features into transformed features
+    
+    Args:
+        inputs: map from feature keys to raw features.
+    
+    Return:
+        outputs: map from feature keys to transformed features.    
+    """
+    outputs = {}
+    outputs[transformed_name(FEATURE_KEY)] = tf.strings.lower(inputs[FEATURE_KEY])
+    outputs[transformed_name(LABEL_KEY)] = tf.cast(inputs[LABEL_KEY], tf.int64)
+    return outputs
